5857cfa5171d77988bbeba4c362195a3d3a377b9
Cooper Cook <36387492+coopertcook@users.noreply.github.com>
2026-02-11 18:16:00 -0800

    Finish first draft of Lab 3 code

--- Lab 3/get_func.py
+++ Lab 3/get_func.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from typing import Any
 import numpy as np
 
 
@@ -8,84 +9,138 @@ def get_func(params: dict[str, float]):
     params = deepcopy(params)
 
     # Extract parameters from dict
-    theta = params["theta"]
-    ampl = params["amplitude"]
-    freq = params["frequency"] * 2 * np.pi  # Convert from Hz to rad/s
-    leng = params["l_arm"]
-    mass = params["m_mass"]
-    g = params["g"]
+    g = params["g"]         # Gravitational constant (m/s^2)
+    U = params["U"]         # Vehicle speed in x-direction (m/s)
+    
+    # Masses
+    m_tot = params["m_tot"]     # Actuated mass (kg)
+    msmus = params["msmus"]     # Sprung to unsprung mass ratio
+    m_us = m_tot / (1 + msmus)  # Unsprung mass (kg)
+    m_s = m_tot - m_us          # Sprung mass (kg)
+    m_a = 0.02 * m_s            # Actuator mass (kg)
+    
+    # Passive Stiffness
+    w_s = params["w_s"]     # Suspension natural frequency (rad/s)
+    k_s = m_s * w_s**2      # Suspension stiffness (N/m)
+    w_wh = params["w_wh"]   # Tire wheel hop frequency (rad/s)
+    k_t = m_us * w_wh**2    # Tire stiffness (N/m)
 
-    # Get initial condition (add position into the system so solve_ivp can integrate it for us)
-    px = 0
-    initial = [px, theta]
+    # Passive Damping
+    zeta_s = params["zeta_s"]       # Passive damping ratio
+    b_s = 2 * zeta_s * w_s * m_s    # Suspension damping (N·s/m)
+
+    # Actuator - Passive
+    w_a = 2 * np.pi * 5         # Actuator frequency (rad/s)
+    k_a = m_a * w_a**2          # Actuator stiffness (N/m)
+    b_a = 2 * 0.1 * w_a * m_a   # Actuator damping (N·s/m)
+
+    # Actuator - Active
+    zeta_c = params["zeta_c"]       # Active damping ratio
+    T_c = params["T_c"]             # Voice coil coupling constant (N/A)
+    R_c = params["R_c"]             # Voice coil winding resistance (Ohms)
+    b_c = 2 * zeta_c * w_s * m_s    # Voice coil damping (N·s/m)
+
+    # Road
+    road_delta_x = params["road_delta_x"]       # Distance between randomly generated road heights (m)
+    road_length = params["road_length"]         # Road length (m)
+    road_max_slope = params["road_max_slope"]   # Maximum instantaneous slope (dy/dx) of road
+    X_i = np.arange(0, road_length+road_delta_x, road_delta_x)  # Generate road position vector
+    road_slope_raw = np.random.random(np.shape(X_i))    # Generate random slope vector
+    slope_i = road_max_slope * (road_slope_raw - np.mean(road_slope_raw)) # Scale and subtract mean from road slope vector
+    def slope_interpolator(x: float):
+        return np.interp(x, X_i, slope_i)
+
+    # Get initial condition
+    initial = [
+        0,  # Actuator mass momentum
+        0,  # Sprung mass momentum
+        0,  # Unsprung mass momentum
+        (m_a) * g / k_a,                # Actuator displacement
+        (m_a + m_s) * g / k_s,          # Suspension displacement
+        (m_a + m_s + m_us) * g / k_t,   # Tire displacement
+        ]
 
     # Create derivative calculator function to be used by solve_ivp()
     # state = [px, theta]
-    def func(t, state):
+    def func(t: float, state: list[float]):
         state = deepcopy(state)
-        ps = state[0]
-        pus = state[1]
-        pa = state[2]
-        qs = state[3]
-        qt = state[4]
-        qa = state[5]
+        p_a = state[0]  # Actuator mass momentum
+        p_s = state[1]  # Sprung mass momentum
+        p_us = state[2] # Unsprung mass momentum
+        q_a = state[3]  # Actuator displacement
+        q_s = state[4]  # Suspension displacement
+        q_t = state[5]  # Tire displacement
 
-        # Sine and cosine of theta, for computational speed
-        sin_theta = np.sin(theta)
-        cos_theta = np.cos(theta)
-        tan_theta = np.tan(theta)
+        # Position of road and its derivatives
+        slope_interp = deepcopy(slope_interpolator)
+        X = U * t   # vehicle velocity x time
+        Y = np.zeros_like(X, dtype=float)
+        dYdX = slope_interp(X)
 
-        # Position of cart and its derivatives
-        X = 0
-        Y = ampl * np.sin(freq * t)
-        d_Y = ampl * freq * np.cos(freq * t)
-        dd_Y = -(freq**2) * Y
+        # Velocities
+        v_a = p_a / m_a     # Actuator mass velocity
+        v_s = p_s / m_s     # Sprung mass velocity
+        v_us = p_us / m_us  # Unsprung mass velocity
+        v_in = U * dYdX            # Tire input velocity
+
+        # Voice coil current
+        i_c = b_c * v_s / T_c   # Voice coil input current (A)
+        e_c = R_c * i_c         # Voice coil input voltage (V)
+        P_c = i_c * e_c         # Voice coil power (W)
+        
+        # Forces
+        F_g_a = -m_a * g    # Actuator mass gravitational force
+        F_g_s = -m_s * g    # Sprung mass gravitational force
+        F_g_us = -m_us * g  # Unsprung mass gravitational force
+
+        F_b_a = b_a * (v_s - v_a)   # Actuator damping force
+        F_s_a = k_a * q_a           # Actuator spring force
+        F_vc = T_c * i_c            # Voice coil force
+        F_a = F_b_a + F_s_a + F_vc  # Total actuator force
+        
+        F_b_s = b_s * (v_us - v_s)  # Suspension damping force
+        F_s_s = k_s * q_s           # Suspension spring force
+        F_s = F_b_s + F_s_s         # Total suspension force
+        
+        # Tire force
+        if q_t <= 0:
+            F_t = 0
+        else:
+            F_t = k_t * q_t
 
         # State derivatives
-        d_theta = px / (leng * cos_theta * mass)
-        d_px = (
-            mass * g * sin_theta * cos_theta
-            + mass * dd_Y * sin_theta * cos_theta
-            - tan_theta * d_theta * px
-        )
-
-        # Other state variables
-        x = X + leng * np.sin(theta)  # x-position of pendulum mass
-        y = Y + leng * np.cos(theta)  # y-position of pendulum mass
-        py = mass * (d_Y - tan_theta * px / mass)  # y-momentum of pendulum mass
-        d_py = mass * (
-            dd_Y - tan_theta * d_px / mass - px * d_theta / (mass * cos_theta**2)
-        )  # first time-derivative of y-momentum of pendulum mass
-
+        d_p_a = F_g_a + F_a
+        d_p_s = F_g_s + F_s - F_a
+        d_p_us = F_g_us + F_t - F_s
+        d_q_a = v_s - v_a
+        d_q_s = v_us - v_s
+        d_q_t = v_in - v_us
+        
         # Concatenate state derivatives
-        d_state = [d_px, d_theta]
+        d_state: list[float] = [
+            d_p_a,
+            d_p_s,
+            d_p_us,
+            d_q_a,
+            d_q_s,
+            d_q_t, # Don't worry, this is a float, not an NDArray
+            ]
 
         # Create current state dict
-        state = {}
-        state["t"] = t
-        state["X"] = X
-        state["Y"] = Y
-        state["x"] = x
-        state["y"] = y
-        state["px"] = px
-        state["py"] = py
-        state["theta"] = theta
-        state["d_Y"] = d_Y
-        state["d_px"] = d_px
-        state["d_py"] = d_py
-        state["d_theta"] = d_theta
-        state["dd_Y"] = dd_Y
-        state["tan_theta"] = tan_theta
+        state_ext: dict[str, float] = {}
+        state_ext["t"] = t
+        state_ext["X"] = X
+        state_ext["Y"] = Y # Don't worry, this is a float, not an NDArray
 
-        return d_state, state
+        return d_state, state_ext
 
-    def func_wrap(t, state):
+    def func_wrap(t: float, state: list[float]):
         """
         Since solve_ivp() needs a function that only returns the state
         derivatives, we create a wrapper for func() that discards the rest of
         the state and returns only the state derivatives
         """
-        d_state, state = func(t, state)
+        d_state, _ = func(t, state)
         return d_state
 
     return func, func_wrap, initial
