56daa7a48892127fe8deed4ea4d865a86e81acaa
Cooper Cook <36387492+coopertcook@users.noreply.github.com>
2026-03-03 12:00:14 -0800

    Add Lab 4 parameters

--- Lab 4/main.py
+++ Lab 4/main.py
@@ -12,31 +12,37 @@ params: dict[str, float] = {}
 # Constants
 params["g"] = 9.8  # gravity (m/s^2)
 
-# Mass
-params["m_tot"] = 3000.0 / 2.2  # Total vehicle mass (lbs -> kg)
-params["msmus"] = 5.0           # Sprung to unsprung mass ratio
-params["mams"] = 0.04           # Actuator to sprung mass ratio
+# Flywheel and piston rod
+params["m_fw"] = 10 / 2.2       # Flywheel mass (lbs -> kg)
+params["R"] = 4 * 0.0254        # Flywheel rod joint radius (in -> m)
+params["L"] = 4 * params["R"]   # Rod length (m)
+params["J_fw"] = params["m_fw"] * params["R"]**2 / 2    # Flywheel moment of inertia (kg * m^2)
 
-# Damping ratios
-params["zeta_s"] = 0.7  # Passive damping ratio
-params["zeta_c"] = 1.7  # Active damping ratio
+# Piston
+params["D_p"] = 4 * 0.0254                      # Piston diameter (in -> m)
+params["A_p"] = np.pi * params["D_p"]**2 / 4    # Piston area (m^2)
 
-# Suspension
-params["w_a"] = 2 * np.pi * 5   # Actuator natural frequency (rad/s)
-params["w_s"] = 2 * np.pi * 1.2 # Suspension natural frequency (Hz -> rad/s)
-params["w_wh"] = 2 * np.pi * 8  # Tire / wheel hop frequency (Hz -> rad/s)
+# Air
+params["V_st"] = params["A_p"] * 2 * params["R"]    # Volume displaced due to stroke (m^3)
+params["V_TDC"] = 80 / 10**6                        # Volume remaining at top of stroke (cc -> m^3)
+params["V_0"] = params["V_st"] + params["V_TDC"]    # Volume at BDC (m^3)
+params["P_0"] = 1 * 10**5                           # Atmospheric pressure (atm -> Pa)
 
-# Voice coil parameters
-params["R_c"] = 0.005           # Winding resistance (Ohm)
-params["T_c"] = 1.0             # Coupling constant (N/A)
+# Controller
+params["w_fw_des"] = 1500 / 60 * 2 * np.pi  # Desired flywheel rotational speed (RPM -> rad/s)
+params["K_p"] = 1   # Controller proportional gain
 
-# Vehicle velocity
-params["U"] = 40 * 0.46 # Vehicle speed (mph -> m/s)
+# Valves
+params["d_o"] = 0.5 * 0.0254      # Outlet check-valve flow diameter (in -> m)
+params["d_i"] = 0.5 * 0.0254      # Inlet check-valve flow diameter (in -> m)
+params["A_o_nom"] = np.pi * params["d_o"]**2 / 4  # Outlet check-valve flow area (m)
+params["A_i_nom"] = np.pi * params["d_i"]**2 / 4  # Inlet check-valve flow area (m)
 
-# Road
-params["road_delta_x"] = 0.5        # Distance between randomly generated road heights (m)
-params["road_length"] = 500         # Road length (m)
-params["road_max_slope"] = 0.005    # Maximum instantaneous slope (dy/dx) of road
+# Accumulator
+params["rho"] = 1.28                    # Air density at atmospheric pressure (kg/m^3)
+params["c"] = 340                       # Air speed of sound (m/s)
+params["V_acc"] = 5 * params["V_0"]     # Accumulator volume (m^3)
+params["C_acc"] = params["V_acc"] / (params["rho"] * params["c"]**2)    # Accumulator compliance (m^4*s/kg)
 
 # Time
 t_start: float = 0
@@ -49,10 +55,7 @@ t_eval = np.arange(min(t_span), max(t_span)+t_increment, t_increment)
 
 # Run simulations
 solutions: list[str, dict[str, float]] = []
-params["zeta_c"] = 0
 solutions.append(solve_problem(params, t_eval, "baseline"))
-params["zeta_c"] = 0.7
-solutions.append(solve_problem(params, t_eval, "zeta_c=0.7"))
 
 # ani = plot_ani(solutions)
 fig = plot_time(solutions)
