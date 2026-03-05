Fix lots of bugs; Change pressure calculation to standard PV^gamma=const. relationship; Finish multi-axis plotting

Lab 4/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from solve_problem import solve_problem
-from plot import plot_time, plot_ani
+from plot import plot_2d, plot_ani
 plt.rcParams["animation.html"] = "jshtml"
 
 # -----------------------------
@@ -44,8 +44,8 @@ params["C_acc"] = params["V_acc"] / (params["rho"] * params["c"]**2)    # Accumu
 
 # Time
 t_start: float = 0
-t_end: float = 1
-t_increment: float = 0.001
+t_end: float = 0.2
+t_increment: float = 0.0001
 
 t_span = (t_start, t_end)
 t_eval = np.arange(min(t_span), max(t_span)+t_increment, t_increment)
@@ -56,7 +56,7 @@ solutions: list[str, dict[str, float]] = []
 solutions.append(solve_problem(params, t_eval, "baseline"))
 
 # ani = plot_ani(solutions)
-fig = plot_time(solutions)
+fig = plot_2d(solutions, [("x", "theta"), ("d_x", "w_fw")], "X vs. Theta")
 # plt.axis('equal')
 
 # fig = plot_time(solutions)

Lab 4/plot.py
@@ -1,54 +1,92 @@
 from typing import Any
 from matplotlib.axes import Axes
 from matplotlib.lines import Line2D
+from matplotlib.colors import hsv_to_rgb
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import numpy as np
 import pandas as pd
 
 
-def plot_2d(solutions, plot_keys: list[tuple[str, str]]):
+def plot_2d(
+    solutions: list[dict[str, float] | dict[str, pd.DataFrame]], 
+    plot_keys: list[tuple[str, str]], 
+    title: str
+    ):
+
     fig = plt.figure()
+
+    hue = np.linspace(0, 1, len(plot_keys), endpoint=False)
+    sat = np.linspace(1, 0.2, len(solutions))
+    val = 0.8
 
     # ---- Generate axes for each key pair ----
     axes: dict[tuple[str, str], Axes] = {}  # Keys and their associated axis
     x_keys: dict[str, list[Axes]] = {}
     y_keys: dict[str, list[Axes]] = {}
     keys_dict: dict[str, list[float]] = {}  # Consolidated data
-    for keys in plot_keys:
+    for i, keys in enumerate(plot_keys):
         assert keys not in axes.keys(), f"Redundant plotting keys '{keys}'. Each plotting key tuple should be unique."
         if keys[0] in x_keys.keys():
+            # If x-key has been seen before, make a copy of the Axes associated with it
             seen_ax = x_keys[keys[0]][0]
             axes[keys] = seen_ax.twinx()
+            x_keys[keys[0]].append(axes[keys])
+            y_keys[keys[1]].append(axes[keys])
         else:
             if keys[1] in y_keys.keys():
-                seen_ax = x_keys[keys[0]][0]
+                # If y-key has been seen before, make a copy of the Axes associated with it
+                seen_ax = y_keys[keys[1]][0]
                 axes[keys] = seen_ax.twiny()
-            else:
+                x_keys[keys[0]].append(axes[keys])
+                y_keys[keys[1]].append(axes[keys])
+            elif i > 0:
+                phantom_x_axis = axes[plot_keys[0]].twinx()
+                axes[keys] = phantom_x_axis.twiny()
+                x_keys[keys[0]] = [phantom_x_axis]
+                y_keys[keys[1]] = [axes[keys]]
+            else: 
+                # If neither key in key-pair have been seen before, 
+                # make a new plot and create new entries for the keys
                 axes[keys] = fig.add_subplot()
+                x_keys[keys[0]] = [axes[keys]]
+                y_keys[keys[1]] = [axes[keys]]
 
-        x_keys[keys[0]].append(axes[keys])
-        y_keys[keys[1]].append(axes[keys])
 
-    assert len(x_keys.keys()) > 2, f"Too many x-axes for 2D plot: {x_keys}. Maximum is 2."
-    assert len(y_keys.keys()) > 2, f"Too many y-axes for 2D plot: {y_keys}. Maximum is 2."
+    assert len(x_keys.keys()) <= 2, f"Too many x-axes for 2D plot: {x_keys}. Maximum is 2."
+    assert len(y_keys.keys()) <= 2, f"Too many y-axes for 2D plot: {y_keys}. Maximum is 2."
 
-    for solution in solutions:
+    for j, solution in enumerate(solutions):
         name = solution["name"]
         df: pd.DataFrame = solution["data"]
 
         # Collect data for all referenced keys in solution
-        for key in keys_dict.keys():
+        for key in (x_keys.keys() | y_keys.keys()):
             keys_dict[key] = df.get(key).to_numpy()
 
-        for (x_key, y_key) in plot_keys:
-        acc.plot(t_vals, a_s_vals, label=f"sprung mass acceleration: {name}")
-        acc.plot(t_vals, dYdX_vals, label=f"dYdX: {name}")
-        # theta_ax.plot(ts, tan_theta_vals, label="tan_theta")
-    acc.legend()
-    acc.set_title("Acceleration vs. Time")
-    acc.set_xlabel("time (s)")
-    acc.set_ylabel("accleration (m/s^2)")
+        for i, keys in enumerate(plot_keys):
+            axes[keys].plot(
+                keys_dict[keys[0]], keys_dict[keys[1]], 
+                label=f"{keys[0]} vs. {keys[1]}: {name}", 
+                c=hsv_to_rgb([hue[i], sat[j], val])
+                )
+    
+    # Add axis labels for each key on each axis
+    for i, key in enumerate(x_keys):
+        x_keys[key][0].set_xlabel(f"{key}")
+        if i == 1: 
+            x_keys[key][0].xaxis.tick_top()
+            x_keys[key][0].xaxis.set_label_position("top")
+    for i, key in enumerate(y_keys):
+        y_keys[key][0].set_ylabel(f"{key}")
+        if i == 1: 
+            y_keys[key][0].yaxis.tick_right()
+            y_keys[key][0].yaxis.set_label_position("right")
+
+    # Set plot title using a single axis
+    axes[plot_keys[0]].set_title(title, y=1.12)
+    fig.legend() # Add legend
+    fig.tight_layout()
     return fig
 
 

Lab 4/state_func.py
@@ -52,27 +52,28 @@ def get_func(params: dict[str, float]):
         s["w_fw"] = s["p_fw"] / s["J_fw"]                               # Flywheel angular velocity
         s["x"] = np.sqrt(s["L"]**2 - s["R"]**2 * sth**2) - s["R"] * cth # Piston position
         s["d_x"] = m_theta * s["w_fw"]                                  # Piston velocity (dx/dtheta * dtheta/dt)
+        s["V"] = s["A_p"] * (s["L"] + s["R"] - s["x"]) + s["V_TDC"]              # Cylinder volume (m^3)
 
         #----- Air -------------------------------------------
         # Pressures
-        s["P"] = s["P_0"] * (1 / (1 - s["q_air"] / s["V_0"])**s["gamma"] - 1)   # Cylinder pressure
+        s["P"] = s["P_0"] * (s["V"]/s["q_air"])**s["gamma"]   # Cylinder pressure
         s["P_acc"] = s["q_acc"] / s["C_acc"]    # Accumulator pressure
         
         # Delta pressures
-        del_P_i = 0 - s["P"]    # Pressure delta at inlet check-valve
+        del_P_i = s["P_0"] - s["P"]    # Pressure delta at inlet check-valve
         del_P_o  = s["P"] - s["P_acc"]  # Pressure delta at accumulator check-valve
         
         # Check valve areas
-        s["A_i"] = 0 if del_P_i <= 0 else s["A_i"]  # Inlet check-valve area correction
-        s["A_o"] = 0 if del_P_i <= 0 else s["A_o"]  # Accumulator check-valve area correction
+        s["A_i"] = 0 if del_P_i <= 0 else s["A_i_nom"]  # Inlet check-valve area correction
+        s["A_o"] = 0 if del_P_i <= 0 else s["A_o_nom"]  # Accumulator check-valve area correction
         
         # Inlet/accumulator flow rates
-        Q_i = s["A_i"] * np.sqrt(2 / s["rho"] * np.abs(del_P_i)) * np.sign(del_P_i) # Inlet air flow rate
-        Q_o = s["A_o"] * np.sqrt(2 / s["rho"] * np.abs(del_P_o)) * np.sign(del_P_o) # Accumulator air flow rate
+        s["Q_i"] = s["A_i"] * np.sqrt(2 / s["rho"] * np.abs(del_P_i)) * np.sign(del_P_i) # Inlet air flow rate
+        s["Q_o"] = s["A_o"] * np.sqrt(2 / s["rho"] * np.abs(del_P_o)) * np.sign(del_P_o) # Accumulator air flow rate
         
         # State flow rates
-        s["d_q_air"] = 1 / s["A_p"] * m_theta * (s["p_fw"] / s["J_fw"]) + Q_i - Q_o # Cylinder air flow rate
-        s["d_q_acc"] = Q_o                                                          # Accumulator air flow rate
+        s["d_q_air"] = s["A_p"] * m_theta * (s["p_fw"] / s["J_fw"]) + s["Q_i"] - s["Q_o"]   # Cylinder air flow rate
+        s["d_q_acc"] = s["Q_o"]                                                             # Accumulator air flow rate
 
         #----- Flywheel controller and acceleration ----------
         s["tau_in"] = s["K_p"] * (s["w_fw_des"] - s["w_fw"])    # Controller torque
@@ -85,7 +86,6 @@ def get_func(params: dict[str, float]):
             s["d_q_acc"],
             s["d_p_fw"], # Don't worry, this is a float, not an NDArray
             ]
-
         return d_state, s
 
     def func_wrap(t: float, state: list[float]):